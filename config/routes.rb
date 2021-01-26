f9105ba6b827f545faee3db7ca37d5abd4c69be1
domaincrisman <cmanzanore@gmail.com>
2021-01-26 18:55:50 +0100

    users index and show

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:index, :show]
 end
