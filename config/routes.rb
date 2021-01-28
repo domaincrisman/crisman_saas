e89de96ac14d0dab88511984bdffd64588284e96
domaincrisman <cmanzanore@gmail.com>
2021-01-28 20:45:55 +0100

    if the user has not accepted the invitation, he can be re-invited

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/index'
   root 'home#index'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   
-  resources :users, only: [:index, :show]
+  resources :users, only: [:index, :show] do
+    member do
+      patch :resend_invitation
+    end
+  end
   
   resources :tenants do
     get :my, on: :collection
