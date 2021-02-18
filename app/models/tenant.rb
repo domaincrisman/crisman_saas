913bc66e3c6890f4ea6c45b06bfb862ec9d20605
domaincrisman <cmanzanore@gmail.com>
2021-02-18 15:49:19 +0100

    global and tenant-specific views

--- app/models/tenant.rb
+++ app/models/tenant.rb
@@ -1,7 +1,8 @@
 class Tenant < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
-
+  validates :name, length: { in: 2..20 }
+  
   has_many :members
   has_many :users, through: :members
 
