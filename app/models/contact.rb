638a654ed091468a71117cf762b74ebde8b8cc45
domaincrisman <cmanzanore@gmail.com>
2021-03-09 22:44:46 +0100

    Add multitenancy scope Contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,8 +1,9 @@
 class Contact < ApplicationRecord
-  belongs_to :tenant
+  #belongs_to :tenant
+  acts_as_tenant(:tenant)
 
   validates :first_name, :last_name, :phone_number, :email, presence: true
   #validates_uniqueness_to_tenant :first_name, :last_name, :phone_number, :email
   validates_uniqueness_of :phone_number, scope: :tenant_id
-  
+
 end
