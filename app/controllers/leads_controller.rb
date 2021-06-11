bd71627c17b316b81519b8aa2ef72d4ffdcabe2a
domaincrisman <cmanzanore@gmail.com>
2021-06-11 12:28:17 +0200

    path to create Contact for current_tenant without current_user

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -2,10 +2,11 @@ class LeadsController < ApplicationController
   skip_before_action :authenticate_user!
   layout  "leads_layout"
 
-  before_action :set_tenant, only: [:new, :show]
+  before_action :set_tenant, only: [:new, :show, :create]
   set_current_tenant_through_filter
 
   def new
+    @contact = Contact.new
   end
   
   def show
@@ -13,9 +14,27 @@ class LeadsController < ApplicationController
     render 'contacts/show'
   end
   
+  def create
+    @contact = Contact.new(contact_params)
+    respond_to do |format|
+      if @contact.save
+        format.html { redirect_to tenant_lead_path(@tenant, @contact), notice: 'Contact was successfully created.' }
+        format.json { render :show, status: :created, location: @contact }
+      else
+        format.html { render :new }
+        format.json { render json: @contact.errors, status: :unprocessable_entity }
+      end
+    end
+
+  end
   private
-  def set_tenant
-    @tenant = Tenant.find(params[:tenant_id])
-    set_current_tenant(@tenant)
+
+    def set_tenant
+      @tenant = Tenant.find(params[:tenant_id])
+      set_current_tenant(@tenant)
+    end
+
+    def contact_params
+      params.require(:contact).permit(:first_name, :last_name, :phone_number, :email)
     end
 end
